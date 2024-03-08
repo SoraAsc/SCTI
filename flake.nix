b9c49f6a7124062889a002f25a58189cbec81f50
Emanuel Peixoto Gomes <86881881+EmanuelPeixoto@users.noreply.github.com>
2024-03-08 18:25:52 +0000

    Criado flake.nix para fácil reprodução

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  outputs = { nixpkgs, ... }: {
+    devShells.x86_64-linux.default = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+  	    config.allowUnfree = true;
+      };
+    in
+      pkgs.mkShell {
+        name = "SCTI-Web";
+        packages = with pkgs; [
+          go
+        ]
+      };
+  };
+}
