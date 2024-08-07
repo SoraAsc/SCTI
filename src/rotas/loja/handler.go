dce7ef004ebd10ab2aff8bd33e643e3ddb77a80f
MintzyG <ericbraga2000@gmail.com>
2024-08-07 17:53:30 -0300

    Rotas consertadas e proteção por login adicionada

--- src/rotas/loja/handler.go
+++ src/rotas/loja/handler.go
@@ -10,3 +10,8 @@ type Handler struct{}
 func (h *Handler) GetLoja(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Loja")
 }
+
+func RegisterRoutes(mux *http.ServeMux) {
+  handler := &Handler{}
+  mux.HandleFunc("GET /loja", handler.GetLoja)
+}
