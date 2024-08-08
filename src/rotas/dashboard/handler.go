d82a5872d17859e869ad68312c06869d0ca4a1a4
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-08-07 22:35:24 -0300

    Dashboard skeleton

--- src/rotas/dashboard/handler.go
+++ src/rotas/dashboard/handler.go
@@ -1,8 +1,8 @@
 package dashboard
 
 import (
-  "fmt"
   "net/http"
+  "SCTI/fileserver"
 )
 
 type Handler struct{}
@@ -19,10 +19,27 @@ func (h *Handler) GetDashboard(w http.ResponseWriter, r *http.Request) {
     // fmt.Println("Invalid accessToken")
     http.Redirect(w, r, "/login", http.StatusSeeOther)
   }
-  fmt.Fprintf(w, "User ID: %v", auth.Value)
+  var t = fileserver.Execute("template/dashboard.gohtml")
+  t.Execute(w, nil)
+  // fmt.Fprintf(w, "User ID: %v", auth.Value)
 }
 
 func RegisterRoutes(mux *http.ServeMux) {
   handler := &Handler{}
   mux.HandleFunc("GET /dashboard", handler.GetDashboard)
+  mux.HandleFunc("GET /logoff", handler.GetLogoff)
 }
+func(h *Handler) GetLogoff(w http.ResponseWriter, r *http.Request) {
+    http.SetCookie(w, &http.Cookie{
+      Name:   "accessToken",
+      Value:  "",
+      MaxAge: -1,
+      Secure: false,
+      HttpOnly: true,
+      Path: "/",
+      SameSite: http.SameSiteLaxMode,
+    })
+    http.Redirect(w, r, "/login", http.StatusSeeOther)
+
+    w.WriteHeader(http.StatusOK)
+  }
