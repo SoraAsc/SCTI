51e464b5387d80276d538a7be3be5ec55f2acc0f
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2024-08-10 20:27:34 -0300

    Dashboard POST

--- src/rotas/dashboard/handler.go
+++ src/rotas/dashboard/handler.go
@@ -1,11 +1,22 @@
 package dashboard
 
 import (
-  "net/http"
   "SCTI/fileserver"
+  "encoding/json"
+  "fmt"
+  "log"
+  "net/http"
 )
 
 type Handler struct{}
+
+type Courses struct {
+  Seg string `json:"seg"`
+  Ter string `json:"ter"`
+  Qua string `json:"qua"`
+  Qui string `json:"qui"`
+  Sex string `json:"sex"`
+}
 
 func (h *Handler) GetDashboard(w http.ResponseWriter, r *http.Request) {
   auth, err := r.Cookie("accessToken")
@@ -24,22 +35,49 @@ func (h *Handler) GetDashboard(w http.ResponseWriter, r *http.Request) {
   // fmt.Fprintf(w, "User ID: %v", auth.Value)
 }
 
+func (h *Handler) PostDashboard(w http.ResponseWriter, r *http.Request) {
+  var courses Courses
+  if r.Header.Get("Content-Type") == "application/json" {
+    err := json.NewDecoder(r.Body).Decode(&courses)
+    if err != nil {
+      log.Printf("Error decoding JSON: %v", err)
+      http.Error(w, "Invalid JSON", http.StatusBadRequest)
+      return
+    }
+  } else {
+    // Lida com application/x-www-form-urlencoded
+    if err := r.ParseForm(); err != nil {
+      log.Printf("Error parsing form: %v", err)
+      http.Error(w, "Invalid form data", http.StatusBadRequest)
+      return
+    }
+    courses.Seg = r.FormValue("seg")
+    courses.Ter = r.FormValue("ter")
+    courses.Qua = r.FormValue("qua")
+    courses.Qui = r.FormValue("qui")
+    courses.Sex = r.FormValue("sex")
+    fmt.Fprintf(w, "%s\n%s\n%s\n%s\n%s", courses.Seg, courses.Ter, courses.Qua, courses.Qui, courses.Sex)
+
+  }
+}
+
 func RegisterRoutes(mux *http.ServeMux) {
   handler := &Handler{}
   mux.HandleFunc("GET /dashboard", handler.GetDashboard)
+  mux.HandleFunc("POST /dashboard", handler.PostDashboard)
   mux.HandleFunc("GET /logoff", handler.GetLogoff)
 }
 func(h *Handler) GetLogoff(w http.ResponseWriter, r *http.Request) {
-    http.SetCookie(w, &http.Cookie{
-      Name:   "accessToken",
-      Value:  "",
-      MaxAge: -1,
-      Secure: false,
-      HttpOnly: true,
-      Path: "/",
-      SameSite: http.SameSiteLaxMode,
-    })
-    http.Redirect(w, r, "/login", http.StatusSeeOther)
+  http.SetCookie(w, &http.Cookie{
+    Name:   "accessToken",
+    Value:  "",
+    MaxAge: -1,
+    Secure: false,
+    HttpOnly: true,
+    Path: "/",
+    SameSite: http.SameSiteLaxMode,
+  })
+  http.Redirect(w, r, "/login", http.StatusSeeOther)
 
-    w.WriteHeader(http.StatusOK)
-  }
+  w.WriteHeader(http.StatusOK)
+}
