c832df12627e1c6125659ab8a593973f6aca3e90
BMSales <bruno.m.sales03@gmail.com>
2024-08-20 17:55:03 -0300

    Juntados handlers de admin em um só arquivo

--- src/rotas/dashboard/admin.go
+++ src/rotas/dashboard/admin.go
@@ -1,9 +1,10 @@
 package dashboard
 
 import (
-  DB "SCTI/database"
-  "fmt"
+	"fmt"
+  "strconv"
   "net/http"
+  DB "SCTI/database"
 )
 
 func SetAdmin(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +16,31 @@ func SetAdmin(w http.ResponseWriter, r *http.Request) {
   }
 
 	fmt.Printf("Success")
+}
+
+func ActiError(w http.ResponseWriter, err error) {
+  w.Header().Set("Content-Type", "text/html")
+  w.Write([]byte(`
+      <div>
+          Falha ao enviar o email de verificação.
+    ` + err.Error() + `
+      </div>
+  `))
+}
+
+func PostActivity(w http.ResponseWriter, r* http.Request) {
+  var a DB.Activity
+  a.Spots, _ = strconv.Atoi(r.FormValue("spots"))
+  a.Activity_type = r.FormValue("type")
+  a.Room = r.FormValue("room")
+  a.Speaker = r.FormValue("speaker")
+  a.Topic = r.FormValue("topic")
+  a.Description = r.FormValue("description")
+  a.Time = r.FormValue("time")
+  a.Day, _ = strconv.Atoi(r.FormValue("day"))
+
+  _, err := DB.CreateActivity(a)
+  if err != nil {
+    ActiError(w, err)
+  }
 }
