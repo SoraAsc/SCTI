426e1155c96b4680087ee1e6d4ed4ea5549bceca
GitHub Action <action@github.com>
2024-09-12 19:51:50 +0000

    Auto-format Go code

--- src/rotas/dashboard/ingresso.go
+++ src/rotas/dashboard/ingresso.go
@@ -9,23 +9,23 @@ import (
 )
 
 func GetIngresso(w http.ResponseWriter, r *http.Request) {
-  cookie, err := r.Cookie("accessToken")
-  if err != nil {
-    http.Redirect(w, r, "/login", http.StatusSeeOther)
-    return
-  }
-  if cookie.Value == "-1" {
-    http.Redirect(w, r, "/login", http.StatusSeeOther)
-    return
-  }
+	cookie, err := r.Cookie("accessToken")
+	if err != nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+	if cookie.Value == "-1" {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
 
-  email := DB.GetEmail(cookie.Value)
-  standing := DB.GetStanding(email)
-  if !standing {
-    Erros.HttpError(w, "dashboard/ingresso", fmt.Errorf("Usuário não possui email verificado"))
-    return
-  }
+	email := DB.GetEmail(cookie.Value)
+	standing := DB.GetStanding(email)
+	if !standing {
+		Erros.HttpError(w, "dashboard/ingresso", fmt.Errorf("Usuário não possui email verificado"))
+		return
+	}
 
-  var t = fileserver.Execute("template/ingresso.gohtml")
-  t.Execute(w, nil)
+	var t = fileserver.Execute("template/ingresso.gohtml")
+	t.Execute(w, nil)
 }
