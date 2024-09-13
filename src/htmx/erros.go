d8f9853dccae2975aa9382e6c0471374187d3187
JuliannaRkg544 <juliana.rkg544@gmail.com>
2024-09-13 12:28:18 -0300

    botão de entrar adicionado a  header e criaão de um modal para mensagens das responses

--- src/htmx/erros.go
+++ src/htmx/erros.go
@@ -7,18 +7,31 @@ import (
 func Failure(w http.ResponseWriter, message string, err error) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(`
+  <div class="modal">
   <div class="error-messages">
   ` + message + `
   ` + err.Error() + `
   </div>
+   <span class="close" onclick=" const modals = document.querySelectorAll('.modal');
+        if (modals.length > 0) {
+            modals[modals.length - 1].style.display = 'none';
+        }">x</span>
+  </div>
+
   `))
 }
 
 func Success(w http.ResponseWriter, message string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Write([]byte(`
+  <div class="modal">
   <div class = "success-messages">
   ` + message + `
+  </div>
+  <span class="close" onclick=" const modals = document.querySelectorAll('.modal');
+        if (modals.length > 0) {
+            modals[modals.length - 1].style.display = 'none';
+        }">x</span>
   </div>
   `))
 }
